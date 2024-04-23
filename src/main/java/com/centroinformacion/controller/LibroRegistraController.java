f80da4964552e407a3d4394b15e526ae27d8d7f8
MARCO-viera <marcosamsung942@gmail.com>
2024-04-23 12:15:31 -0500

    Commit

--- src/main/java/com/centroinformacion/controller/LibroRegistraController.java
+++ src/main/java/com/centroinformacion/controller/LibroRegistraController.java
@@ -1,15 +1,5 @@
 package com.centroinformacion.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.centroinformacion.service.DataCatalogoService;
-import com.centroinformacion.service.PaisService;
-
 public class LibroRegistraController {
-	/*@Autowired
-	private PaisService paisService;
-
-	@Autowired
-	private DataCatalogoService dataCatalogoService;
-	*/
+	/**/
 }
