96d869e7c6742152388f2cf78cc5126dcaebd6ce
Nando21MS <manriquef400@gmail.com>
2024-04-25 16:05:07 -0500

    AVANCE DE TESIS 1.1

--- src/main/java/com/centroinformacion/service/TesisServiceImp.java
+++ src/main/java/com/centroinformacion/service/TesisServiceImp.java
@@ -3,10 +3,12 @@ package com.centroinformacion.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.centroinformacion.entity.Tesis;
 import com.centroinformacion.repository.TesisRepository;
 
+@Service
 public class TesisServiceImp implements TesisService{
 	
 	@Autowired	
