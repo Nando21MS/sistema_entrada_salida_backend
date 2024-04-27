43ac4f1456d510598f6d0eb5a5fb54acd438dbc1
Nando21MS <manriquef400@gmail.com>
2024-04-27 13:06:40 -0500

    TESIS - FINALIZADO

--- src/main/java/com/centroinformacion/repository/TesisRepository.java
+++ src/main/java/com/centroinformacion/repository/TesisRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.centroinformacion.entity.Tesis;
 
 public interface TesisRepository extends JpaRepository<Tesis, Integer> {
-
 }
