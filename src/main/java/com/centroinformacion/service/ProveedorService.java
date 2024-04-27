4d0ed99d083b1f4335d77f04ab1edcfac432df0c
18luzD <Luz18cabanillaspizarro@gmail.com>
2024-04-26 20:08:15 -0500

    Proveedor-Finalizado

---
+++ src/main/java/com/centroinformacion/service/ProveedorService.java
@@ -0,0 +1,9 @@
+package com.centroinformacion.service;
+import java.util.List;
+
+import com.centroinformacion.entity.Proveedor;
+public interface ProveedorService {
+	public abstract Proveedor insertaActualizaProveedor(Proveedor obj);
+	public abstract List<Proveedor> listaTodos();
+
+}
