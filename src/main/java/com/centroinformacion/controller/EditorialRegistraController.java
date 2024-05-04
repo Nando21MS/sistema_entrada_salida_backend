0446eab5c00469d7a4c169635a5866aa2f59997e
Jans110718 <jansyaredjade111@gmail.com>
2024-05-04 12:14:57 -0500

    Cambios

--- src/main/java/com/centroinformacion/controller/EditorialRegistraController.java
+++ src/main/java/com/centroinformacion/controller/EditorialRegistraController.java
@@ -13,30 +13,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.centroinformacion.entity.Tesis;
-import com.centroinformacion.service.TesisService;
+import com.centroinformacion.entity.Editorial;
+import com.centroinformacion.service.EditorialService;
 import com.centroinformacion.util.AppSettings;
-
-
 @RestController
-@RequestMapping("/url/tesis")
+@RequestMapping("/url/editorial")
 @CrossOrigin(origins = AppSettings.URL_CROSS_ORIGIN)
-public class TesisRegistraController {
-	
+public class EditorialRegistraController {
 	@Autowired
-	private TesisService TesisService;
+	private EditorialService editorialService;
 	
 	@GetMapping
 	@ResponseBody
-	public ResponseEntity<List<Tesis>> listaeTesis(){
-		List<Tesis> lista = TesisService.listaTesis();
+	public ResponseEntity<List<Editorial>> listaeEditorial(){
+		List<Editorial> lista = editorialService.listaTodos();
 		return ResponseEntity.ok(lista);
 	}
 	
 	@PostMapping
 	@ResponseBody
-	public ResponseEntity<?> inserta(@RequestBody Tesis obj){
+	public ResponseEntity<?> inserta(@RequestBody Editorial obj){
 		HashMap<String, Object> salida = new HashMap<>();
 		
 		obj.setFechaActualizacion(new Date());
@@ -44,11 +40,11 @@ public class TesisRegistraController {
 		obj.setEstado(AppSettings.ACTIVO);
 		
 		
-		Tesis objSalida = TesisService.insertaActualizaTesis(obj);
+		Editorial objSalida = editorialService.insertaActualizaEditorial(obj);
 		if (objSalida == null) {
 			salida.put("mensaje","Error en el registro");
 		}else {
-			salida.put("mensaje","Se registró la Tesis con el ID ==> " + objSalida.getIdTesis());
+			salida.put("mensaje","Se registró la Editorial con el ID ==> " + objSalida.getIdEditorial());
 		}
 		return ResponseEntity.ok(salida);
 	}
