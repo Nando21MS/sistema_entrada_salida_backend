6a4469969ca225baabf8eedbf75d311c22e28e4c
Jans110718 <jansyaredjade111@gmail.com>
2024-05-08 21:42:26 -0500

    CRUD-EditorialFinalizado

--- src/main/java/com/centroinformacion/service/EditorialService.java
+++ src/main/java/com/centroinformacion/service/EditorialService.java
@@ -2,7 +2,19 @@ package com.centroinformacion.service;
 
 import java.util.List;
 import com.centroinformacion.entity.Editorial;
+
 public interface EditorialService {
+//Para el crud  
 	public abstract Editorial insertaActualizaEditorial(Editorial obj);
+
+	public abstract List<Editorial> listaEditorialPorRazonSocialLike(String razonSocial);
+
+	public abstract void eliminaEditorial(int idEditorial);
+
 	public abstract List<Editorial> listaTodos();
+
+	public abstract List<Editorial> listaEditorialPorRazonSocialIgualRegistra(String razonSocial);
+
+	public abstract List<Editorial> listaEditorialPorRazonSocialIgualActualiza(String razonSocial, int idEditorial);
+
 }
--- src/main/java/com/centroinformacion/service/EditorialServiceImp.java
+++ src/main/java/com/centroinformacion/service/EditorialServiceImp.java
@@ -1,24 +1,46 @@
 package com.centroinformacion.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.centroinformacion.entity.Editorial;
 import com.centroinformacion.repository.EditorialRepository;
 
 @Service
 public class EditorialServiceImp implements EditorialService {
-
 	@Autowired
 	private EditorialRepository repository;
+
 	@Override
 	public Editorial insertaActualizaEditorial(Editorial obj) {
 		return repository.save(obj);
 	}
+
 	@Override
 	public List<Editorial> listaTodos() {
 		return repository.findByOrderByRazonSocialAsc();
-	}	
+	}
+
+	@Override
+	public List<Editorial> listaEditorialPorRazonSocialLike(String razonSocial) {
+		return repository.listaPorRazonSocialLike(razonSocial);
+	}
+
+	@Override
+	public void eliminaEditorial(int idEditorial) {
+		repository.deleteById(idEditorial);
+	}
+
+	@Override
+	public List<Editorial> listaEditorialPorRazonSocialIgualRegistra(String razonSocial) {
+// TODO Auto-generated method stub
+		return repository.listaPorRazonSocialIgualRegistra(razonSocial);
+	}
+
+	@Override
+	public List<Editorial> listaEditorialPorRazonSocialIgualActualiza(String razonSocial, int idEditorial) {
+// TODO Auto-generated method stub 
+		return repository.listaPorRazonSocialIgualActualiza(razonSocial, idEditorial);
+	}
+
 }
