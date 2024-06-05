1be4b2b88f439efb9bb8c7956d926c19b45df298
AstridYovera <ayoveratinoco@gmail.com>
2024-06-04 20:14:30 -0500

    pc3 - avance

--- src/main/java/com/centroinformacion/controller/AutorConsultaController.java
+++ src/main/java/com/centroinformacion/controller/AutorConsultaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.centroinformacion.entity.Autor;
 import com.centroinformacion.service.AutorService;
+import com.centroinformacion.util.AppSettings;
 
 @RestController
 @RequestMapping("/url/consultaAutor")
+@CrossOrigin(origins = AppSettings.URL_CROSS_ORIGIN)
 public class AutorConsultaController {
 	@Autowired
 	private AutorService autorService;
