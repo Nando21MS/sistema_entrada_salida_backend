b246435f86be57150befd9e7cb1f7cfe61cc587f
Nando21MS <manriquef400@gmail.com>
2024-10-20 13:36:16 -0500

    CBO

--- src/main/java/com/centroinformacion/controller/UtilController.java
+++ src/main/java/com/centroinformacion/controller/UtilController.java
@@ -1,14 +1,27 @@
 package com.centroinformacion.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.centroinformacion.entity.TipoDocumento;
+import com.centroinformacion.service.TipoDocService;
 import com.centroinformacion.util.AppSettings;
 
 @RestController
 @RequestMapping("/url/util")
 @CrossOrigin(origins = AppSettings.URL_CROSS_ORIGIN)
 public class UtilController {
-
+	private TipoDocService tipoDocService;
+	
+	@GetMapping("/listaTipoDoc")
+	@ResponseBody
+	public List<TipoDocumento> listaTipoDoc() {
+		return tipoDocService.listaTodos();
+	}
 }
---
+++ src/main/java/com/centroinformacion/service/TipoDocService.java
@@ -0,0 +1,9 @@
+package com.centroinformacion.service;
+
+import java.util.List;
+
+import com.centroinformacion.entity.TipoDocumento;
+
+public interface TipoDocService {
+	public abstract List<TipoDocumento> listaTodos();
+}
