28258153e39aacf0c1273e15c0c736c05cf12160
AstridYovera <ayoveratinoco@gmail.com>
2024-10-20 16:21:08 -0500

    avance

---
+++ src/main/java/com/centroinformacion/entity/Proveedor.java
@@ -0,0 +1,68 @@
+package com.centroinformacion.entity;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "proveedor")
+public class Proveedor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idProveedor;
+	private String razonSocial;
+	private String ruc;
+	private String desProd;
+	private String nombres;
+	private String apellidos;
+	private String cargoRes;
+	
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idTipoDoc")
+	private TipoDocumento tipoDocumento;
+	
+	private String nroDoc;
+	private int estado;	
+	
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idUsuarioRegistro")
+	private Usuario usuarioRegistro;
+	
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idUsuarioActualiza")
+	private Usuario usuarioActualiza;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date fechaRegistro;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date fechaActualizacion;
+	
+	public String getReporteEstado() {
+		return estado == 1 ? "Activo" : "Inactivo";
+	}
+	public String getTipoDocumento() {
+		return tipoDocumento.getDescripcion();
+	}
+}
