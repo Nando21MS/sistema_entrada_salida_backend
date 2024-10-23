2e1b74cd1f4281c0a8aa53c4a6c0a1f9182f729c
LionelOrh <lionelorihuelac@gmail.com>
2024-10-23 18:36:26 -0500

    Feat:Validaciones

--- src/main/java/com/centroinformacion/service/impl/PersonaExternaServiceImpl.java
+++ src/main/java/com/centroinformacion/service/impl/PersonaExternaServiceImpl.java
@@ -1,14 +1,16 @@
-package com.centroinformacion.service;
+package com.centroinformacion.service.impl;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.centroinformacion.entity.Externo;
 import com.centroinformacion.repository.PersonaExternaRepository;
+import com.centroinformacion.service.PersonaExternaService;
 
 @Service
 public class PersonaExternaServiceImpl implements PersonaExternaService{
-
 	@Autowired
 	private PersonaExternaRepository repository;
 	
@@ -17,5 +19,13 @@ public class PersonaExternaServiceImpl implements PersonaExternaService{
 		
 		return repository.save(obj);
 	}
+
+	@Override
+	public List<Externo> listaNumDoc(String num_doc) {
+		// TODO Auto-generated method stub
+		 return repository.listaPorDniIgualRegistra(num_doc);
+	}
+
+	
 	
 }
