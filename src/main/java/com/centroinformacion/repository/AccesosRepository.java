5f2a9a6f6e59d5f0919800490c4df44d469b989f
Nando21MS <manriquef400@gmail.com>
2024-10-25 04:56:35 -0500

    ARRGLOS PARA MIS ACCESOS

--- src/main/java/com/centroinformacion/repository/AccesosRepository.java
+++ src/main/java/com/centroinformacion/repository/AccesosRepository.java
@@ -10,23 +10,25 @@ import com.centroinformacion.entity.Accesos;
 import com.centroinformacion.entity.Rol;
 
 public interface AccesosRepository extends JpaRepository<Accesos, Integer> {
-    List<Accesos> findByCodigo(String codigo);
+    List<Accesos> findByUsuarioRegistrado_Login(String login); // Modificado para buscar por usuario
+
     List<Accesos> findByRol(Rol idRol);
+
     List<Accesos> findByFecha(Date fecha);
 
     @Query("select a from Accesos a where "
-            + " a.codigo like ?1 and "
+            + " a.usuarioRegistrado.login like ?1 and "
             + " a.fecha >= ?2 and "
             + " a.fecha <= ?3 and "
-            + " (?4 = -1 or a.rol.idRol = ?4) ")
+            + " (?4 = -1 or a.rol.idRol = ?4)")
     List<Accesos> listaConsultaCompleja(
-            String codigo, 
-            Date fechaDesde, 
+            String login,
+            Date fechaDesde,
             Date fechaHasta,
             int idRol);
 
     // Para apartado "Mis Accesos" de la app móvil
-    @Query("select a from Accesos a where a.usuarioRegistro.login = ?1 and "
+    @Query("select a from Accesos a where a.usuarioRegistrado.login = ?1 and "
             + "(?2 is null or a.fecha = ?2)")
     List<Accesos> findByUsuarioLoginAndFecha(String login, Date fecha);
 }
